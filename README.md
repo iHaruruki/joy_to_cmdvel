feea3cc5f03013f783d0f15db198588f7ffe021a
Haruki Isono <haruki.isono861@gmail.com>
2025-01-17 12:04:36 +0900

    add: 高速モードを追加

--- README.md
+++ README.md
@@ -15,6 +15,18 @@ ros2 run joy joy_node
 ```
 ros2 run joy_to_cmdvel joy_to_cmdvel_node
 ```
+3. 操作方法
+* 左アナログスティック(LS)
+    * 上：[twist.linear.x ++] 前進
+    * 下：[twist.linear.x --] 後退
+    * 右：[twist.linear.y --] 右移動
+    * 左：[twist.linear.y ++] 左移動
+* 右アナログスティック(RS)
+    * 右：[twist.angular.z --] 右旋回（時計回り）
+    * 左：[twist.angular.z ++] 左旋回（反時計回り）
+* R1 button
+    * OFF(msg->buttons[5] == 0)：通常モード
+    * ON (msg->buttons[5] == 1)：高速モード<span style="color:red;">（速度を通常モードの1.5倍にする）<span>
 ### 参考資料
 [joy_node]に関する説明
 * [joy](https://docs.ros.org/en/humble/p/joy/index.html)
--- src/joy_to_cmdvel.cpp
+++ src/joy_to_cmdvel.cpp
@@ -15,7 +15,7 @@ public:
     // /cmd_vel トピックへパブリッシュするパブリッシャの作成
     cmd_vel_pub_ = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
 
-    RCLCPP_INFO(this->get_logger(), "joy_to_cmd_vel ノードを起動しました");
+    RCLCPP_INFO(this->get_logger(), "joy_to_cmd_vel_node has been started.");
   }
 
 private:
@@ -24,13 +24,23 @@ private:
     // geometry_msgs::msg::Twist メッセージの作成
     auto twist = geometry_msgs::msg::Twist();
 
-    // 左スティックの上下（axes[1]）で線形速度、右スティックの左右（axes[3]）で角速度を制御
-    twist.linear.x  = 0.5 * msg->axes[1];  // 前後の移動
-    twist.linear.y  = 0.5 * msg->axes[0];  // 横の移動
+    //---通常モード---//
+    // 左スティックの上下（axes[1]）で前後移動、左スティックの左右（axes[0]）で横移動、右スティックの左右（axes[3]）で角速度を制御
+    twist.linear.x  = 0.3 * msg->axes[1];  // 前後の移動
+    twist.linear.y  = 0.3 * msg->axes[0];  // 横の移動
     twist.linear.z  = 0.0;
     twist.angular.x = 0.0;
     twist.angular.y = 0.0;
-    twist.angular.z = 0.5 * msg->axes[3];  // 旋回
+    twist.angular.z = 0.3 * msg->axes[3];  // 旋回
+
+    //---高速モード---//
+    // R1ボタン（buttons[5]）が押されている場合は速度を通常モードの1.5倍にする
+    if (msg->buttons[5] == 1) {
+        RCLCPP_INFO(this->get_logger(), "高速モード");
+        twist.linear.x  *= 1.5;
+        twist.linear.y  *= 1.5;
+        twist.angular.z *= 1.5;
+    }
 
     // /cmd_vel トピックへパブリッシュ
     cmd_vel_pub_->publish(twist);
